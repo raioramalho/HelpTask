2e5755d4c5750f75d39efaf26203da1130caa417
Alan Ramalho <ramalho.sit@gmail.com>
2022-11-18 15:28:46 -0300

    upload

--- include/banco.php
+++ include/banco.php
@@ -1,8 +1,8 @@
 <?php 
 // renomeie o nome "helpdesk2" para o nome que vc vai dar para o banco.
-	$con =@ mysqli_connect("localhost","phpmyadmin","r4nd0m","help") or die("<h3>Erro de conexão ao banco de dados, favor informar ao suporte técnico.</h3>");
+	$con =@ mysqli_connect("localhost","phpmyadmin","password","help") or die("<h3>Erro de conexão ao banco de dados, favor informar ao suporte técnico.</h3>");
 	
-	$conn =@ mysqli_connect("localhost","phpmyadmin","r4nd0m","help") or die("<h3>Erro de conexão ao banco de dados, favor informar ao suporte técnico.</h3>");
+	$conn =@ mysqli_connect("localhost","phpmyadmin","password","help") or die("<h3>Erro de conexão ao banco de dados, favor informar ao suporte técnico.</h3>");
 
 
  ?>
